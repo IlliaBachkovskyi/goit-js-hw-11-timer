db450cae11e56596ced012e1d62435beca405eeb
IlliaBachkovskyi <illiabachkovskyi@gmail.com>
2021-10-27 11:41:19 +0300

    v1.0

---
+++ js/index.js
@@ -0,0 +1,46 @@
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.init();
+
+    this.days = document.querySelector('[data-value = "days"]');
+    this.hours = document.querySelector('[data-value = "hours"]');
+    this.minutes = document.querySelector('[data-value = "minutes"]');
+    this.seconds = document.querySelector('[data-value = "seconds"]');
+  }
+
+  init() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      const time = this.targetDate - currentTime;
+      this.changeTimer(this.getTimeComponents(time));
+    }, 1000);
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const minutes = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const seconds = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, minutes, seconds };
+  }
+
+  changeTimer({ days, hours, minutes, seconds }) {
+    this.days.textContent = `${days}`;
+    this.hours.textContent = `${hours}`;
+    this.minutes.textContent = `${minutes}`;
+    this.seconds.textContent = `${seconds}`;
+  }
+}
+
+const Countdown = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Nov 19, 2021"),
+});
